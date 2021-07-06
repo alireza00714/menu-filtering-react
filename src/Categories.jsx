b6897710188a93307201573602bf82d42c58df99
Alireza Pirmozhdehi <zootel.land@gmail.comi>
2021-07-06 11:22:40 +0430

    menu filtering implemented

---
+++ src/Categories.jsx
@@ -0,0 +1,19 @@
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => (
+        <button
+          key={index}
+          className="filter-btn"
+          onClick={() => {
+            filterItems(category);
+          }}
+        >
+          {category}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
